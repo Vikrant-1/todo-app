bf00bd7edc6ba1adbfb6451cd527b0a569a40a86
vikrant22 <vikrant@usefreebird.com>
2024-08-19 23:16:05 +0530

    create structure for auth midleware

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+const userAuthMiddleware = (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
+
+
+export {
+    userAuthMiddleware,
+}
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -7,14 +7,15 @@ import {
   updatePassword,
   updateUser,
 } from "../controllers/userController";
+import { userAuthMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 router.post("signup/", registerUser);
 router.post("login/", loginUser);
-router.put("update-password/", updatePassword);
-router.put("avatar/", updateAvatar);
-router.put("user/", updateUser);
-router.get("user/", getUser);
+router.put("update-password/", userAuthMiddleware, updatePassword);
+router.put("avatar/", userAuthMiddleware, updateAvatar);
+router.put("user/", userAuthMiddleware, updateUser);
+router.get("user/", userAuthMiddleware, getUser);
 
 export const userRoutes = router;
